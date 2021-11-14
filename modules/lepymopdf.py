2a162b0dfe9edda177dbcb642f696440f1e6f179
kjuraszek <donkrissocorleone@gmail.com>
2021-11-14 16:38:14 +0100

    Update lepymopdf.py
    Use  _PDF_FORMATS constant

--- modules/lepymopdf.py
+++ modules/lepymopdf.py
@@ -9,6 +9,8 @@ from collections import Counter
 from PIL import Image
 from fpdf import FPDF
 
+from modules.utilities import _PDF_FORMATS
+
 
 class LePyMoPDF(FPDF):
     """LePyMoPDF class"""
@@ -17,8 +19,8 @@ class LePyMoPDF(FPDF):
         """Init LePyMoPDF class"""
         super().__init__()
         self._abort = 0
-        self.pdf_width = 210
-        self.pdf_height = 297
+        self.pdf_width = _PDF_FORMATS["A4"]["pdf_width"]
+        self.pdf_height = _PDF_FORMATS["A4"]["pdf_height"]
         self.image_src = Image.open(image_src)
         self.image_scaled = image_scaled
         self.start_date = start_date
@@ -28,32 +30,42 @@ class LePyMoPDF(FPDF):
     def main_page(self):
         """Generates main page of PDF"""
         self.add_page()
+        y_pos = _PDF_FORMATS["A4"]["page_y_pos"]
         self.big_header("LePyMo")
-        self.small_header("Lego Python Mosaic", 20)
+        self.small_header("Lego Python Mosaic", y_pos)
 
-        self.set_xy(self.pdf_width / 2 - 30, 25)
-        self.image(self.image_scaled, link='', type='', w=60, h=60)
+        y_pos += _PDF_FORMATS["A4"]["small_header_margin"]
+        self.set_xy((self.pdf_width  - _PDF_FORMATS["A4"]["main_page_image_size"]) // 2, y_pos)
+        self.image(self.image_scaled, link='', type='', w = _PDF_FORMATS["A4"]["main_page_image_size"], h = _PDF_FORMATS["A4"]["main_page_image_size"])
 
-        self.medium_header("Build instructions for your image", 95)
-        self.small_header(f"Dimensions: {self.image_width} x {self.image_height} bricks", 105)
-        self.small_header(f"Bricks in total: {self.image_width * self.image_height}", 110)
-        self.medium_header("Bricks by color:", 120)
-        y_pos = 130
+        y_pos += _PDF_FORMATS["A4"]["medium_header_margin"] + _PDF_FORMATS["A4"]["main_page_image_size"]
+        self.medium_header("Build instructions for your image", y_pos)
+
+        y_pos += _PDF_FORMATS["A4"]["medium_header_margin"]
+        self.small_header(f"Dimensions: {self.image_width} x {self.image_height} bricks", y_pos)
+
+        y_pos += _PDF_FORMATS["A4"]["small_header_margin"]
+        self.small_header(f"Bricks in total: {self.image_width * self.image_height}", y_pos)
+
+        y_pos += _PDF_FORMATS["A4"]["medium_header_margin"]
+        self.medium_header("Bricks by color:", y_pos)
+
+        y_pos += _PDF_FORMATS["A4"]["medium_header_margin"]
 
         for color, qty in self.colors.items():
             if self._abort == 1:
                 return
-            if y_pos + 8 > 280:
+            if y_pos + _PDF_FORMATS["A4"]["small_brick_margin"] > _PDF_FORMATS["A4"]["page_max_y_pos"]:
                 self.add_page()
-                y_pos = 10
-            self.small_brick(75, y_pos - 4, color)
-            self.brick_text(f"x {qty} {color}", 85, y_pos)
-            y_pos += 8
+                y_pos = _PDF_FORMATS["A4"]["medium_header_margin"]
+            self.small_brick(_PDF_FORMATS["A4"]["small_brick_x_pos"], y_pos - _PDF_FORMATS["A4"]["small_brick_margin"] / 2, color)
+            self.brick_text(f"x {qty} {color}", _PDF_FORMATS["A4"]["small_brick_text_x_pos"], y_pos)
+            y_pos += _PDF_FORMATS["A4"]["small_brick_margin"]
 
-        y_pos += 10
-        if y_pos + 30 > 280:
+        y_pos += _PDF_FORMATS["A4"]["medium_header_margin"]
+        if y_pos + _PDF_FORMATS["A4"]["medium_header_font_size"] > _PDF_FORMATS["A4"]["page_max_y_pos"]:
             self.add_page()
-            y_pos = 20
+            y_pos = _PDF_FORMATS["A4"]["page_y_pos"]
 
         self.big_header("Have fun!", y_pos)
 
@@ -89,76 +101,78 @@ class LePyMoPDF(FPDF):
         self.footer()
         self.big_header("Step " + str(step + 1))
         row_colors = Counter(list(self.image_src.getdata())[step * self.image_width:(step + 1) * self.image_width])
-        y_pos = 25
+
+        y_pos = _PDF_FORMATS["A4"]["page_y_pos"] + _PDF_FORMATS["A4"]["small_header_margin"]
         self.small_header("You'll need in this step:", y_pos)
-        y_pos += 5
+        y_pos += _PDF_FORMATS["A4"]["small_header_margin"]
 
         for color, qty in row_colors.items():
             if self._abort == 1:
                 return
-            if y_pos + 8 > 280:
+            if y_pos + _PDF_FORMATS["A4"]["small_brick_margin"] > _PDF_FORMATS["A4"]["page_max_y_pos"]:
                 self.add_page()
                 self.footer()
-                y_pos = 10
-            self.small_brick(75, y_pos - 4, color)
-            self.brick_text(f"x {qty} {color}", 85, y_pos)
-            y_pos += 8
+                y_pos = _PDF_FORMATS["A4"]["medium_header_margin"]
+            self.small_brick(_PDF_FORMATS["A4"]["small_brick_x_pos"], y_pos - _PDF_FORMATS["A4"]["small_brick_margin"] / 2, color)
+            self.brick_text(f"x {qty} {color}", _PDF_FORMATS["A4"]["small_brick_text_x_pos"], y_pos)
+            y_pos += _PDF_FORMATS["A4"]["small_brick_margin"]
 
-        y_pos += 10
+        y_pos += _PDF_FORMATS["A4"]["medium_header_margin"]
         self.small_header("Bricks from left to the right:", y_pos)
-        y_pos += 5
+
+        y_pos += _PDF_FORMATS["A4"]["small_header_margin"]
         for current_row_element in current_row:
             if self._abort == 1:
                 return
-            if y_pos + 8 > 280:
+            if y_pos + _PDF_FORMATS["A4"]["small_brick_margin"] > _PDF_FORMATS["A4"]["page_max_y_pos"]:
                 self.add_page()
                 self.footer()
-                y_pos = 10
-            self.small_brick(75, y_pos - 4, current_row_element["color"])
-            self.brick_text(f"x {current_row_element['count']} {str(current_row_element['color'])}", 85, y_pos)
-            y_pos += 8
+                y_pos = _PDF_FORMATS["A4"]["medium_header_margin"]
+            self.small_brick(_PDF_FORMATS["A4"]["small_brick_x_pos"], y_pos - _PDF_FORMATS["A4"]["small_brick_margin"] / 2, current_row_element["color"])
+            self.brick_text(f"x {current_row_element['count']} {str(current_row_element['color'])}", _PDF_FORMATS["A4"]["small_brick_text_x_pos"], y_pos)
+            y_pos += _PDF_FORMATS["A4"]["small_brick_margin"]
 
-    def big_header(self, header_text, from_top=12):
+    def big_header(self, header_text, from_top = 12):
         """Adds a big header to the current page"""
-        self.set_font('Arial', '', 38)
+        self.set_font('Arial', '', _PDF_FORMATS["A4"]["big_header_font_size"])
         self.text((self.pdf_width - self.get_string_width(header_text)) / 2, from_top, header_text)
 
-    def medium_header(self, header_text, from_top=12):
+    def medium_header(self, header_text, from_top = 12):
         """Adds a medium header to the current page"""
-        self.set_font('Arial', '', 24)
+        self.set_font('Arial', '', _PDF_FORMATS["A4"]["medium_header_font_size"])
         self.text((self.pdf_width - self.get_string_width(header_text)) / 2, from_top, header_text)
 
-    def small_header(self, header_text, from_top=12):
+    def small_header(self, header_text, from_top = 12):
         """Adds a small header to the current page"""
-        self.set_font('Arial', '', 14)
+        self.set_font('Arial', '', _PDF_FORMATS["A4"]["small_header_font_size"])
         self.text((self.pdf_width - self.get_string_width(header_text)) / 2, from_top, header_text)
 
     def footer(self):
         """Adds a small footer to the current page with link to project's Github"""
         if self.page_no() > 1:
             footer_text = "Generated using LePyMo"
-            self.set_font('Arial', '', 10)
+            self.set_font('Arial', '', _PDF_FORMATS["A4"]["footer_font_size"])
             self.set_text_color(222, 222, 222)
-            self.text((self.pdf_width - self.get_string_width(footer_text)) / 2, 294, footer_text)
-            self.link(x=(self.pdf_width - self.get_string_width(footer_text)) / 2, y=290,
-                      w=self.get_string_width(footer_text), h=5,
+            self.text((self.pdf_width - self.get_string_width(footer_text)) / 2, _PDF_FORMATS["A4"]["footer_text_y_pos"], footer_text)
+            self.link(x=(self.pdf_width - self.get_string_width(footer_text)) / 2, y = _PDF_FORMATS["A4"]["footer_link_y"],
+                      w=self.get_string_width(footer_text), h = _PDF_FORMATS["A4"]["footer_link_height"],
                       link='https://github.com/kjuraszek/lego-python-mosaic/')
             self.set_text_color(0, 0, 0)
-            self.set_font('Arial', '', 16)
+            self.set_font('Arial', '', _PDF_FORMATS["A4"]["paging_font_size"])
             current_page = f'- {self.page_no()} -'
-            self.text((self.pdf_width - self.get_string_width(current_page)) / 2, 290, current_page)
+            self.text((self.pdf_width - self.get_string_width(current_page)) / 2, _PDF_FORMATS["A4"]["paging_y_pos"], current_page)
 
     def brick_text(self, text, position_x, position_y):
         """Adds a brick text to the page"""
-        self.set_font('Arial', '', 14)
+        self.set_font('Arial', '', _PDF_FORMATS["A4"]["small_brick_font_size"])
         self.text(position_x, position_y, text)
 
     def small_brick(self, position_x, position_y, color):
         """Adds a brick to the page"""
         self.set_fill_color(*color)
         self.set_xy(position_x, position_y)
-        self.cell(5, 5, fill=True, border=1)
-        self.ellipse(position_x + 1.0, position_y + 1.0, 3, 3)
+        self.cell(_PDF_FORMATS["A4"]["small_brick_size"], _PDF_FORMATS["A4"]["small_brick_size"], fill = True, border = 1)
+        self.ellipse(position_x + _PDF_FORMATS["A4"]["small_brick_ellipsis_pos"], position_y + _PDF_FORMATS["A4"]["small_brick_ellipsis_pos"], _PDF_FORMATS["A4"]["small_brick_ellipsis_size"], _PDF_FORMATS["A4"]["small_brick_ellipsis_size"])
 
     def abort(self):
         """Stops creating PDF"""
